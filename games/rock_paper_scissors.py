8dd32b94717ec2db9925d77b9934dd3557ae39c5
krishna-waidande-dev <krishnawaidande.dev@gmail.com>
2021-02-23 23:59:48 +0530

    Rewrote the program structure

--- games/rock_paper_scissors.py
+++ games/rock_paper_scissors.py
@@ -1,49 +1,72 @@
 import random
 
+#All Required Variables.
+rock='rock'
+scissors='scissors'
+paper='paper'
+player1=''
+player2=''
+name=''
+totalScore=0
+
+def printGameBanner():
+    print("<==========================================>")
+    print ('Welcome to Rock, Paper, Scissors Game!!')
+    print("<==========================================>")
+
+def takeInputs():
+    print ('Enter your choice (Rock/Paper/Scissors)')
+    player1=input().lower()
+    print ('Your Choice is: ' + player1)
+
+    player2=random.choice([rock, paper, scissors])
+    print ('Computer Choice is: ' + player2)
+    
+    return player1, player2
+
+def isValidInput(player1):
+    if (player1 not in (rock, paper, scissors)):
+        print('Invalid input or spelling mistake..Please specify correct choice')
+        return False
+    return True
+      
+def processInputs(player1, player2, name):
+    global totalScore
+
+    if (player1 == player2):
+        print ('It\'s a tie..Nobody wins.. Try again..')
+        return
+ 
+    result1=checkCondition(player1, player2)
+    if (result1 == 1):
+        print (name + ' Wins!! Hurray')
+        totalScore += 1
+    else:
+        print ('Computer wins!! Better luck next time :) ')
+
 def checkCondition(player1, player2):
-    if (player1 == 'Rock' and player2 == 'Scissors'):
+    if (player1 == rock and player2 == scissors):
         return 1
-    elif (player1 == 'Paper' and player2 == 'Rock'):
+    elif (player1 == paper and player2 == rock):
         return 1
-    elif (player1 == 'Scissors' and player2 == 'Paper'):
+    elif (player1 == scissors and player2 == paper):
         return 1
     else:
         return 0
 
 def main():
-    flag=''
-    player1=''
-    player2=''
-    name=''
-    
-    print ('Welcome to Rock, Paper, Scissors Game!!')
+    continueGame=''
+    printGameBanner()
     print ('Enter your name:')
     name=input()
-
-    while flag != 'no':  
-        print ('Enter your choice (Rock/Paper/Scissors)')
-        player1=input()
-        
-        print ('Your Choice is: ' + player1)
-
-        player2=random.choice(['Rock','Paper','Scissors'])
-        print ('Computer Choice is: ' + player2)
-
-        if (player1 == player2):
-            print ('Its a tie..No one wins')
+    while continueGame != 'no':
+        player1, player2 = takeInputs()
+        if(not(isValidInput(player1))):
             continue
-            
-        result1=checkCondition(player1, player2)
-        result2=checkCondition(player2, player1)
-
-        print ('Result 1: ' + str(result1))
-        print ('Result 2: ' + str(result2))
-
-        if result1 == 1:
-            print (name +' Wins!! Hurray')
-        else:
-            print ('Computer wins!!')
-        
+        processInputs(player1, player2, name)
         print ('Do you want to play again? (yes/no)')
-        flag=input()
+        continueGame=input()
+
+    print("Your Total Score is: " + str(totalScore))
+
 main()
